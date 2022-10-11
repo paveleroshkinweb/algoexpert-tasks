fbc939fc0b65ca61add0f25b7cc409e1859ed17e
paveleroshkinweb <eroshkin321@gmail.com>
2022-10-12 00:16:26 +0300

    add run_length encoding

---
+++ src/run_length_encoding/solution.py
@@ -0,0 +1,29 @@
+def combine_chunks(chunks):
+	encoded_string = ""
+	for char, counter in chunks:
+		if counter <= 9:
+			encoded_string += str(counter) + char
+		else:
+			whole_parts = counter // 9
+			rest = counter % 9
+			for _ in range(whole_parts):
+				encoded_string += '9' + char
+			encoded_string += str(rest) + char
+	return encoded_string
+
+
+def runLengthEncoding(string):
+	idx = 0
+	chunks = []
+	while idx < len(string):
+		current_char = string[idx]
+		current_counter = 0
+		flag = False
+		while idx < len(string) and string[idx] == current_char:
+			current_counter += 1
+			idx += 1
+			flag = True
+		chunks.append((current_char, current_counter))
+		if not flag:
+			idx += 1
+	return combine_chunks(chunks)
