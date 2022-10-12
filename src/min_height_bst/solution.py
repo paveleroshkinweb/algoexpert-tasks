4207fc3a54d314472ba08aec47cf2bdca2cbe3c8
paveleroshkinweb <eroshkin321@gmail.com>
2022-10-13 01:30:40 +0300

    add bst_traversal and min_height

---
+++ src/min_height_bst/solution.py
@@ -0,0 +1,13 @@
+# Time complexity - O(n)
+# Space complexity - O(n)
+def minHeightBst(array):
+    return minHeightBstHelper(array, 0, len(array) - 1)
+
+def minHeightBstHelper(array, left, right):
+	if left > right:
+		return
+	middle = (left + right) // 2
+	new_node = BST(array[middle])
+	new_node.left = minHeightBstHelper(array, left, middle - 1)
+	new_node.right = minHeightBstHelper(array, middle + 1, right)
+	return new_node
