c580d0d9e117f580bf398f06000cd31ca0592818
paveleroshkinweb <eroshkin321@gmail.com>
2022-10-18 01:21:57 +0300

    add next greater element

---
+++ src/next_greater_element/solution.py
@@ -0,0 +1,16 @@
+# Time complexity - O(n)
+# Space complexity -O(n)
+def nextGreaterElement(array):
+	elements = [-1 for _ in range(len(array))]
+	stack = []
+	for i in range(2 * len(array)):
+		index = i % len(array)
+		
+		while stack and array[stack[-1]] < array[index]:
+			top = stack.pop()
+			elements[top] = array[index]
+		
+		if elements[index] == -1:
+			stack.append(index)
+	
+	return elements
