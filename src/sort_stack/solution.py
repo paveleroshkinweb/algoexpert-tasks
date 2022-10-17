ba5c67aec670e80b264098c2be3d462bc940b435
paveleroshkinweb <eroshkin321@gmail.com>
2022-10-17 23:47:54 +0300

    add sortStack problem

---
+++ src/sort_stack/solution.py
@@ -0,0 +1,19 @@
+# Time complexity - O(n^2)
+# Space complexity - O(n)
+def sortStack(stack):
+	if len(stack) <= 1:
+		return stack
+	last_element = stack.pop()
+	sortStack(stack)
+	insertInPlace(stack, last_element)
+	return stack
+
+
+def insertInPlace(stack, element, next_to_insert=None):
+	if len(stack) == 0 or stack[-1] < element:
+		stack.append(element)
+	else:
+		last_element = stack.pop()
+		insertInPlace(stack, element, last_element)
+	if next_to_insert is not None:
+		stack.append(next_to_insert)
