25353f955410bae7e2b1aa1816c7b65b68367c70
paveleroshkinweb <eroshkin321@gmail.com>
2022-10-18 23:13:41 +0300

    add reverse words in string

---
+++ src/reverse_words_in_string/solution.py
@@ -0,0 +1,17 @@
+# Time complexity - O(n)
+# Space complexity - O(n)
+def reverseWordsInString(string):
+    reversed_words = ""
+    i = 0
+    while i < len(string):
+        if string[i] == ' ':
+            reversed_words = string[i] + reversed_words
+            i += 1
+        else:
+            last_word = ''
+            while i < len(string) and string[i] != ' ':
+                last_word += string[i]
+                i += 1
+            reversed_words = last_word + reversed_words
+        
+    return reversed_words
