321230e304432723a22be5302706efd459866a6b
paveleroshkinweb <eroshkin321@gmail.com>
2022-11-02 11:46:03 +0300

    add four number

--- src/four_nomber_sum/solution.py
+++ src/four_nomber_sum/solution.py
@@ -1,14 +1,17 @@
 from collections import defaultdict
 
 
+# Time complexity - O(n^2), worst O(n^3)
+# Space complexity - O(n^2),
 def fourNumberSum(array, targetSum):
-	pairs = defaultdict(lambda: [])
-	quadruplets = []
-	for i in range(1, len(array) - 1):
-		for j in range(i + 1, len(array)):
-			diff = targetSum - array[i] - array[j]
-			for pair in pairs[diff]:
-				quadruplets.append([*pair, array[i], array[j]])
+	quadruplets = defaultdict(lambda: [])
+	sums = []
+	for i in range(1, len(array)-1):
+		for j in range(i+1, len(array)):
+			current_sum = array[i] + array[j]
+			diff = targetSum - current_sum
+			for pair in quadruplets[diff]:
+				sums.append([*pair, array[i], array[j]])
 		for k in range(0, i):
-			pairs[array[k] + array[i]].append([array[k], array[i]])
-	return quadruplets
+			quadruplets[array[k] + array[i]].append([array[k], array[i]])
+	return sums
