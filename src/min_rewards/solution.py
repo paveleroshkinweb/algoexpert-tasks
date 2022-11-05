3a2c95a8824ba3eaa74776c1b4feb18e029b6ec5
paveleroshkinweb <eroshkin321@gmail.com>
2022-11-05 14:16:09 +0300

    add min_rewards

---
+++ src/min_rewards/solution.py
@@ -0,0 +1,47 @@
+def minRewards(scores):
+    assert len(scores) > 0
+    if len(scores) == 1:
+        return 1
+    if len(scores) == 2:
+        return 3
+    is_up = scores[0] < scores[1]
+    idx = 0
+    min_rewards = 0
+    prev_left = 0
+    jumps = 0
+    while idx < len(scores) - 1:
+        if is_up:
+            new_idx = count_up(scores, idx)
+            min_rewards += count(new_idx - idx)
+            prev_left = new_idx - idx
+            if is_local_min(scores, idx):
+                jumps += 1
+        else:
+            new_idx = count_down(scores, idx)
+            min_rewards += count(new_idx - idx) + max(prev_left, new_idx - idx) + 1 
+            prev_left = 0
+        is_up = not is_up
+        idx = new_idx
+    if scores[-1] > scores[-2]:
+        min_rewards += prev_left + 1
+    return min_rewards - jumps
+
+
+def is_local_min(scores, idx):
+    return idx != 0 and scores[idx] < scores[idx-1] and scores[idx] < scores[idx+1]
+
+
+def count_up(scores, idx):
+    while idx < len(scores) - 1 and scores[idx] < scores[idx+1]:
+        idx += 1
+    return idx
+
+
+def count_down(scores, idx):
+    while idx < len(scores) - 1 and scores[idx] > scores[idx+1]:
+        idx += 1
+    return idx
+
+
+def count(n):
+    return (n * (n+1) // 2)
